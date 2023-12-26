e1a67bfac0be11987603acfe8e868a3b66a12b21
milaasHD <saalim.baba@outlook.com>
2023-12-26 16:51:58 +0100

    Second

--- js.js
+++ js.js
@@ -1,14 +1,28 @@
-import('tailwindcss').Config
-
-document.addEventListener("DOMContentLoaded", function (){
-
-    let boy;
-    function start(){
-        fetch("https://random-word-api.vercel.app/api?words=1").then((res) => res.json()).then(
-            json => {boy = json; console.log(boy)})
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    let startButton = document.getElementById("start");
+    if (startButton) {
+        startButton.addEventListener("click", function() {
+            window.location.href = 'main_game.html'; // Redirect to the game page
+        });
     }
-    let start_button = document.getElementById("start")
-    start_button.addEventListener("click", start())
-})
+
+    let guessField = document.getElementById("Guess_Field");
+    if (guessField) {
+        fetch("https://random-word-api.vercel.app/api?words=1")
+            .then(res => res.json())
+            .then(json => {
+                const wordy = json[0];
+
+                let word_array = wordy.split('')
+                let arraytest = []
+                for (i=0; i < word_array.length; i++)(
+                    arraytest.push("_")
+                )
+
+                console.log(word_array)
+                console.log(arraytest)
+                arraytest.join("", arraytest)
+            })
+            .catch(error => console.error('Fetch Error:', error));
+    }
+});
