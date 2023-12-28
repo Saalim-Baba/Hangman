0e4ee275c94c6815c7089439244a3a66f781303c
milaasHD <saalim.baba@outlook.com>
2023-12-28 23:40:28 +0100

    Without CSS completed

--- js.js
+++ js.js
@@ -19,9 +19,56 @@ document.addEventListener("DOMContentLoaded", function () {
                     arraytest.push("_")
                 )
 
-                console.log(word_array)
-                console.log(arraytest)
-                arraytest.join("", arraytest)
+
+                display_array()
+                function display_array(){
+                    console.log(word_array)
+                    console.log(arraytest)
+                    arraytest.forEach(letter => {
+                        const letterElement = document.createElement('span');
+                        letterElement.textContent = letter;
+                        letterElement.classList.add('letter');
+                        guessField.appendChild(letterElement);
+
+                    });
+
+                }
+                function delete_array(){
+                    arraytest.forEach(letter => {
+                        guessField.removeChild(guessField.firstChild)
+
+                    });
+
+                }
+let count = 1
+
+                function useInput() {
+                    if (count < 7){
+                        let next_step = true
+                        let inputField = document.getElementById('myInput');
+                        let inputValue = inputField.value
+                        for (i=0; word_array.length > i; i++){
+                            if (inputValue === word_array[i]){
+                                arraytest[i] = inputValue
+                                delete_array()
+                                display_array()
+                                next_step = false
+                            }
+                        }
+                        if (next_step === true) {
+                            count++
+                            let hangman_img = document.getElementById("hangman")
+                            hangman_img.src = `./stages/stage${count}.png`
+                        }
+                        inputField.value = '';
+                    }
+                    else{
+                        alert("YOU LOST")
+                    }
+                }
+                let button = document.getElementById("guess")
+                button.addEventListener("click", useInput)
+
             })
             .catch(error => console.error('Fetch Error:', error));
     }
